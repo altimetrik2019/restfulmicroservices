cb244b277508dc6f87bed1655c4b46cf0b2851e1
madhankumar <madhan2007.psgtech@gmail.com>
2020-03-04 17:26:13 +0530

    final commit

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-insert into users(id, name, birthdate) values(1, 'ABCD', sysdate());
-insert into users(id, name, birthdate) values(2, 'BCDE', sysdate());
-insert into users(id, name, birthdate) values(3, 'CDEF', sysdate());
-insert into posts(id, description, user_id) values(1, 'My First Post', 1);
-insert into posts(id, description, user_id) values(2, 'My Second Post', 1);
-insert into posts(id, description, user_id) values(3, 'My First Post', 2);
